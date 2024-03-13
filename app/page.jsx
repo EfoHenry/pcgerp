b83340a04519e35f7d4c60981d0a6b7cacb9ec1f
EfoHenry <manteyhenry@gmail.com>
2024-03-13 05:22:03 +0000

    refact: reset entire project

---
+++ app/page.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Homepage = () => {
+  return (
+    <div>
+      <h1>Homepage</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus
+        doloremque ex aliquam molestias at? Explicabo consequuntur commodi rem,
+        doloremque ipsam odio eaque suscipit numquam excepturi voluptatum iusto
+        iste assumenda libero alias tempore omnis id cumque minima! Tenetur a
+        culpa architecto esse id veniam consectetur corrupti minima
+        necessitatibus, explicabo repellat voluptatum.
+      </p>
+    </div>
+  );
+};
+
+export default Homepage;
